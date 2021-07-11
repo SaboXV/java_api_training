1dcdecc3d42aa54e8ab8d0feb8b75b10e0bd62df
Dylan REIS <dylan.reis@epita.fr>
2021-07-11 17:30:16 +0200

    Fin du Projet - Add fire - Add Test

--- src/test/java/fr/lernejo/navy_battle/ServerInfoTest.java
+++ src/test/java/fr/lernejo/navy_battle/ServerInfoTest.java
@@ -9,14 +9,18 @@ class ServerInfoTest {
     @Test
     void testIt() {
         var one = new Info("id", "url", "message");
+        assertEquals("id", one.getId());
         assertEquals("url", one.getUrl());
+        assertEquals("message", one.getMessage());
     }
 
     @Test
     void testJSON() {
         JSONObject in = new JSONObject("{\"id\": \"my_id\", \"url\":\"my_url\",\"message\":\"my_message\"}");
         var v = Info.fromJSON(in);
+        assertEquals("my_id", v.getId());
         assertEquals("my_url", v.getUrl());
+        assertEquals("my_message", v.getMessage());
 
         assertEquals(v.toJSON().toString(), in.toString());
     }
